0f8fcbecbbbb8aa963204430f1be75e7009c17fa
Bryan <bryan.cerdas07@gmail.com>
2025-06-17 18:31:41 -0600

    estructura basica del proyecto

---
+++ ParqueDiversionesProject/src/main/java/com/mycompany/parquediversionesproject/ParqueDiversionesProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.parquediversionesproject;
+
+/**
+ *
+ * @author bryan
+ */
+public class ParqueDiversionesProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
